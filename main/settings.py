6512fd1ab570c54a61c67d2a5819d9ca4272402b
ZhenHaoyun <ZhenHaoyun1213@hotmail.com>
2019-04-24 18:18:18 +0800

    change: theme

--- main/settings.py
+++ main/settings.py
@@ -167,8 +167,6 @@ CMS_LANGUAGES = {
 CMS_TEMPLATES = (
     ## Customize this
     ('fullwidth.html', 'Fullwidth'),
-    ('sidebar_left.html', 'Sidebar Left'),
-    ('sidebar_right.html', 'Sidebar Right')
 )
 
 CMS_PERMISSION = True
